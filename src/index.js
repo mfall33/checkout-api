36157295fe59295509bf0966f126c4d8ad75d4d4
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-03 14:52:29 +0100

    pull command test

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ fastify.register(require('@fastify/cors'), {
 })
 
 fastify.get("/", async (request, reply) => {
-    reply.send(`Welcome to Github Actions Working.. ${APP_NAME} API`);
+    reply.send(`Welcome to GitHub Actions Working.. ${APP_NAME} API`);
 });
 
 fastify.register(require('./Route/Stripe'))
