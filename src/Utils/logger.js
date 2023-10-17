075930f06ecd1189bfba9f03812ac579060b0f48
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-17 15:10:13 +0100

    feat: logging

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const { INFO_LOG_FILE, ERROR_LOG_FILE } = process.env;
 
+console.log(__dirname + "/" + INFO_LOG_FILE);
+
 const output = fs.createWriteStream(INFO_LOG_FILE);
 const errorOutput = fs.createWriteStream(ERROR_LOG_FILE);
 
