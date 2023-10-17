bcac211c6e2d22c016ad9bc902033111ff6c7a4e
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-17 14:14:42 +0100

    fix: loggin paths from .env

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
-const output = fs.createWriteStream('./logs/app.log');
-const errorOutput = fs.createWriteStream('./logs/error.log');
+const { INFO_LOG_FILE, ERROR_LOG_FILE } = process.env;
+
+const output = fs.createWriteStream(INFO_LOG_FILE);
+const errorOutput = fs.createWriteStream(ERROR_LOG_FILE);
 
 const Logger = new console.Console(output, errorOutput);
 
