9ab601a13500dfa659d2fd23003cf101c3698103
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-19 14:46:29 +0100

    fix: adding write mode to logger

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -5,8 +5,13 @@ const { INFO_LOG_FILE, ERROR_LOG_FILE } = process.env;
 console.log("INFO_LOG_FILE: " + INFO_LOG_FILE);
 console.log("ERROR_LOG_FILE: " + ERROR_LOG_FILE);
 
-const output = fs.createWriteStream(INFO_LOG_FILE);
-const errorOutput = fs.createWriteStream(ERROR_LOG_FILE);
+const output = fs.createWriteStream(INFO_LOG_FILE, {
+    flags: 'w'
+});
+
+const errorOutput = fs.createWriteStream(ERROR_LOG_FILE, {
+    flags: 'w'
+});
 
 const Logger = new console.Console(output, errorOutput);
 
