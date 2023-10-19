7ba79ad0d923dcbe3b568bb7d990120302487735
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-19 14:57:57 +0100

    fix: adding programatic chmod

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -5,6 +5,10 @@ const { INFO_LOG_FILE, ERROR_LOG_FILE } = process.env;
 console.log("INFO_LOG_FILE: " + INFO_LOG_FILE);
 console.log("ERROR_LOG_FILE: " + ERROR_LOG_FILE);
 
+fs.chmod(INFO_LOG_FILE, 0o777, () => {
+    console.log("done")
+})
+
 const output = fs.createWriteStream(INFO_LOG_FILE, {
     flags: 'w'
 });
