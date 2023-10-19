e0ada008bd17892ec283d8268642275f241c5075
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-19 14:39:49 +0100

    fix: logging logger

--- src/Utils/logger.js
+++ src/Utils/logger.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 const { INFO_LOG_FILE, ERROR_LOG_FILE } = process.env;
 
-console.log(__dirname + "/" + INFO_LOG_FILE);
+console.log("INFO_LOG_FILE: " + INFO_LOG_FILE);
+console.log("ERROR_LOG_FILE: " + ERROR_LOG_FILE);
 
 const output = fs.createWriteStream(INFO_LOG_FILE);
 const errorOutput = fs.createWriteStream(ERROR_LOG_FILE);
