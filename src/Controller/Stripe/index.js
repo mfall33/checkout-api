d682713fef55cecaa240642997a208b30c52618b
Matthew Fallon <matthewfallon33@gmail.com>
2023-10-19 15:44:46 +0100

    fix: trying to get logger to write

--- src/Controller/Stripe/index.js
+++ src/Controller/Stripe/index.js
@@ -7,16 +7,11 @@ module.exports.webhook = async (request, reply) => {
     // what we need to do is check the status being successful
     // we need to get the customer_id and then retrieve the user and then delete their cart OR move it into orders
 
-    Logger.log(request.body);
-
     try {
         // Stringify the request body
         const requestBodyJson = JSON.stringify(request.body, null, 2);
 
-        // Specify the file path where you want to save the data
-        const filePath = 'stripe_webhook_data.json'; // Change this to your preferred file path
-
-        Logger.log(requestBodyJson);
+        Logger.log(request.body);
 
         reply.send();
 
